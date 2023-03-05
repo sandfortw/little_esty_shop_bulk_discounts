2376b4bd40d784ecd81e571fd40506aef9ff11f0
Weston Sandfort <sandfortw@gmail.com>
2023-03-05 11:48:20 -0700

    create plain ruby files for pulling api

---
+++ lib/holiday_api/holidays.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+require 'pry'
+require './lib/holiday_api/holiday'
+require './lib/holiday_api/holiday_search'
+require './lib/holiday_api/holiday_service'
+
+holiday_array = HolidaySearch.new.holidays 
+holiday_array.each do |holiday|
+  puts holiday.name 
+  puts holiday.date
+end
+
