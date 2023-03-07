6ef6c51b7eaa3d3cf7edb400d7e73853532c92a6
Weston Sandfort <sandfortw@gmail.com>
2023-03-07 10:13:06 -0700

    add model tests for Bulkdiscount.obsolete? method

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -12,4 +12,35 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe '#obsolete?' do
+    before do
+      @merchant = create(:merchant)
+      @bulk_discount1= BulkDiscount.create!(percent_discounted: 20, quantity_threshold: 10, merchant_id: @merchant.id)
+      @bulk_discount2= BulkDiscount.new(percent_discounted: 15, quantity_threshold: 10, merchant_id: @merchant.id) 
+      @bulk_discount3= BulkDiscount.new(percent_discounted: 15, quantity_threshold: 15, merchant_id: @merchant.id) 
+      @bulk_discount4= BulkDiscount.new(percent_discounted: 15, quantity_threshold: 5, merchant_id: @merchant.id)
+      @bulk_discount5= BulkDiscount.new(percent_discounted: 25, quantity_threshold: 5, merchant_id: @merchant.id) 
+      @bulk_discount6= BulkDiscount.new(percent_discounted: 25, quantity_threshold: 10, merchant_id: @merchant.id) 
+      @bulk_discount7= BulkDiscount.new(percent_discounted: 25, quantity_threshold: 15, merchant_id: @merchant.id) 
+    end
+    describe 'tells you whether a new bulk discount is made obsolete by a previously existing bulk discount' do
+      it 'the discount is less than an existing discount at same threshold' do
+        expect(@bulk_discount2.obsolete?).to be(true)
+      end
+
+      it 'the discount is less than an existing discount at larger threshold' do
+        expect(@bulk_discount3.obsolete?).to be(true)
+      end
+      it 'the discount is less than an existing discount at smaller threshold' do
+        expect(@bulk_discount4.obsolete?).to be(false)
+      end
+      it 'discount is greater than an existing discount regardless of threshold' do
+        expect(@bulk_discount5.obsolete?).to be(false)
+        expect(@bulk_discount6.obsolete?).to be(false)
+        expect(@bulk_discount7.obsolete?).to be(false)
+      end
+    end
+   
+  end
 end
