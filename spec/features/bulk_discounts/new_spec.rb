f974e685f7ccdcccfc9ae808a7b28d8c81058979
Weston Sandfort <sandfortw@gmail.com>
2023-03-07 19:16:34 -0700

    change error messages expected

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -39,7 +39,7 @@ describe 'bulk discounts new' do
         click_on 'Submit'
         expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant))
       end
-      expect(page).to have_content('Invalid input')
+      expect(page).to have_content('Percent discounted must be less than 101')
     end
 
     it 'gets a flash message with invalid data (less than 0 quantity)' do
@@ -49,7 +49,7 @@ describe 'bulk discounts new' do
         click_on 'Submit'
         expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant))
       end
-      expect(page).to have_content('Invalid input')
+      expect(page).to have_content('Quantity threshold must be greater than 0')
     end
 
     it 'gets a flash message with invalid data (less than 0 percent discounted)' do
@@ -59,7 +59,7 @@ describe 'bulk discounts new' do
         click_on 'Submit'
         expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant))
       end
-      expect(page).to have_content('Invalid input')
+      expect(page).to have_content('Percent discounted must be greater than 0')
     end
 
     it 'gets a flash message with invalid data (empty fields)' do
@@ -67,7 +67,26 @@ describe 'bulk discounts new' do
         click_on 'Submit'
         expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant))
       end
-      expect(page).to have_content('Invalid input')
+      expect(page).to have_content('Percent discounted can\'t be blank')
+      expect(page).to have_content('Quantity threshold can\'t be blank')
+    end
+
+    describe 'bad discount' do
+      before do
+        @bulk_discount1 = BulkDiscount.create!(percent_discounted: 20, quantity_threshold: 10, merchant_id: @merchant.id)
+        @bulk_discount2 = BulkDiscount.new(percent_discounted: 15, quantity_threshold: 10, merchant_id: @merchant.id)
+        @bulk_discount3 = BulkDiscount.new(percent_discounted: 15, quantity_threshold: 15, merchant_id: @merchant.id)
+        @bulk_discount4 = BulkDiscount.new(percent_discounted: 15, quantity_threshold: 5, merchant_id: @merchant.id)
+        visit new_merchant_bulk_discount_path(@merchant)
+      end
+      it 'gets a flash message if the discount is worse than something that exists' do
+        within('form') do
+          fill_in 'Percent discounted', with: 15
+          fill_in 'Quantity threshold', with: 10
+          click_on 'Submit'
+        end
+        expect(page).to have_content('You may not enter a discount that is superseded by an existing discount.')
+      end
     end
   end
 end
